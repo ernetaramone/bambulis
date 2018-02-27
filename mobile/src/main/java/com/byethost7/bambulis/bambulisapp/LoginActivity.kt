1cd301c956656097f0c71cea16b17c4079b52130
bambulis <ernneta@gmail.com>
2018-02-27 11:31:08 +0200

    Pridetas prisijungimo langas

---
+++ mobile/src/main/java/com/byethost7/bambulis/bambulisapp/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.byethost7.bambulis.bambulisapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
